3aaee849d12e7de23aa40b945131e29799c9bfee
Aryan Raj <aryanraj2713@gmail.com>
2023-07-02 05:05:49 +0530

    Header UI complete

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
 
+
+import Logo from "../assets/leaf.png"
+
 export const Header = () => {
   return (
     <header>
         <div className='logo'>
-            <img src='' alt=''/>
+            <img src={Logo} alt='Logo'/>
             <span>To Do List</span>
+        </div>
+        <div className='themeSelector'>
+            <span className="light activeTheme"></span>
+            <span className="medium activeTheme"></span>
+            <span className="dark activeTheme"></span>
+            <span className="gOne activeTheme"></span>
+            <span className="gTwo activeTheme"></span>
+            <span className="gThree activeTheme"></span>
         </div>
     </header>
   )
