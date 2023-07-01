9f3ac6744c9f4c31a977b335ffe7e95f74b06122
Aryan Raj <aryanraj2713@gmail.com>
2023-07-02 05:12:18 +0530

    AddTask UI completed

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const AddTask = () => {
   return (
-    <div><h1>AddTask</h1></div>
+    <section className='addTask'>
+        <form>
+            <input type='text' name='task' autoComplete='off' placeholder='add Task' maxLength={25}/>
+            <button type='submitt'>ADD</button>
+        </form>    
+    </section>
   )
 }
