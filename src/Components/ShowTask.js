1ea5a1763170b1741ea5dc0908f2e97e3ef51e72
Aryan Raj <aryanraj2713@gmail.com>
2023-07-22 06:18:54 +0530

    trying css

--- src/Components/ShowTask.js
+++ src/Components/ShowTask.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
+
 export const ShowTask = () => {
   return (
-    <section className='showTask'>
-      <div className='head'>
-        <span className='title'>To Do</span>
-        <span className='count'>0</span>
+    <section className="showTask">
+      <div className="head">
+        <span className="title">To Do</span>
+        <span className="count">0</span>
       </div>
+        <button className="clearAll">Clear All</button>
+        <ui>
+          <p>
+            <span className='name'>Task A</span>
+            <span className='time'>Saturday, July 22, 2023, 5:50 PM</span>
+          </p>
+        </ui>
     </section>
   )
 }
